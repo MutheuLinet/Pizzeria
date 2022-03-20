27ea3437d0da82e488d2a76902e5c99d140f1b40
MutheuLinet <muneenilinet@gmail.com>
2022-03-21 00:20:53 +0300

    Add JQUERY to hide some details from the landing page

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $(".btn.more-pizza").hide();
+  $(".btn.check-out").hide();
+  $("#delivery-query").hide();
+  $("#order-confirmation").hide();
+  $("#grand-total").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $("#location-input").hide();
+  $("#location-btn").hide();
+  $("#del-confirmation").hide();
+
+});
+
+    
