ef72a22a87a196a954e8ce09f4e8dc7b3a37b97d
MutheuLinet <muneenilinet@gmail.com>
2022-03-21 00:22:22 +0300

    Add order button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,23 @@ $(document).ready(function() {
   $("#location-btn").hide();
   $("#del-confirmation").hide();
 
+
+  $('.btn.order').click(function(event) {
+      event.preventDefault();
+      let sizeOfPizza = $(".size option:selected").val();
+      let toppingsOfPizza = $(".toppings option:selected").val();
+      let crustOfPizza = $(".crust option:selected").val();
+      let total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+      let order = 1;
+      let grandTotal = 0;
+
+      $(".btn.order").hide();
+      $(".btn.more-pizza").show();
+      $(".btn.check-out").show();
+
+      $("h6").append(order + ")"+ $(".size option:selected").text() + ","+ $(".crust option:selected").text() +","+$(".toppings option:selected").text() +"," +"TOTAL"+"@"+ total);
+
+  });
 });
 
     
